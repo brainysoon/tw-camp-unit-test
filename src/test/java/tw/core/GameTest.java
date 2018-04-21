c6018be4f27165df19eb2e8cb68e935aa28514c6
brainysoon <brainysoon@gmail.com>
2018-04-21 15:14:40 +0800

    [ GameTest ] - Verify should return expected guess result.

--- src/test/java/tw/core/GameTest.java
+++ src/test/java/tw/core/GameTest.java
@@ -1,5 +1,16 @@
 package tw.core;
 
+import org.junit.Before;
+import org.junit.Test;
+import tw.core.generator.AnswerGenerator;
+import tw.core.model.GuessResult;
+import tw.core.model.Record;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
 /**
  * 在GameTest文件中完成Game中对应的单元测试
  */
@@ -7,4 +18,30 @@ package tw.core;
 
 public class GameTest {
 
+    private Answer mockAnswer;
+    private Game game;
+
+    @Before
+    public void setUp() throws Exception {
+
+        AnswerGenerator mockAnswerGenerator = mock(AnswerGenerator.class);
+        mockAnswer = mock(Answer.class);
+        when(mockAnswerGenerator.generate()).thenReturn(mockAnswer);
+
+        game = new Game(mockAnswerGenerator);
+    }
+
+    @Test
+    public void should_return_an_expected_guess_result() {
+        Record record = new Record();
+        record.increaseCurrentNum();
+        record.increaseIncludeOnlyNum();
+        Answer answer = Answer.createAnswer("1 2 3 4");
+
+        when(mockAnswer.check(answer)).thenReturn(record);
+
+        GuessResult expectedGuessResult = new GuessResult("1A1B", answer);
+        assertEquals(expectedGuessResult.getResult(), game.guess(answer).getResult());
+        assertEquals(expectedGuessResult.getInputAnswer(), game.guess(answer).getInputAnswer());
+    }
 }
