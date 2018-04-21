9aea466e532326719db84c01a9e46c7b4f22bd5f
brainysoon <brainysoon@gmail.com>
2018-04-21 13:45:05 +0800

    [ GameControllerTest ] - Verify play trigger gameView.showGameStatus with status when is not continue.

--- src/test/java/tw/controllers/GameControllerTest.java
+++ src/test/java/tw/controllers/GameControllerTest.java
@@ -2,11 +2,13 @@ package tw.controllers;
 
 import org.junit.Before;
 import org.junit.Test;
+import tw.commands.InputCommand;
 import tw.core.Game;
 import tw.views.GameView;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * 在GameControllerTest文件中完成GameController中对应的单元测试
@@ -16,12 +18,14 @@ public class GameControllerTest {
     private GameController gameController;
     private Game mockGame;
     private GameView mockGameView;
+    private InputCommand mockInputCommand;
 
     @Before
     public void setUp() {
 
         mockGame = mock(Game.class);
         mockGameView = mock(GameView.class);
+        mockInputCommand = mock(InputCommand.class);
 
         gameController = new GameController(mockGame, mockGameView);
     }
@@ -31,5 +35,15 @@ public class GameControllerTest {
         gameController.beginGame();
 
         verify(mockGameView).showBegin();
+    }
+
+    @Test
+    public void should_trigger_show_game_status_when_call_play_and_status_is_not_continue() throws Exception {
+        when(mockGame.checkCoutinue()).thenReturn(false);
+        when(mockGame.checkStatus()).thenReturn("status");
+
+        gameController.play(mockInputCommand);
+
+        verify(mockGameView).showGameStatus("status");
     }
 }
