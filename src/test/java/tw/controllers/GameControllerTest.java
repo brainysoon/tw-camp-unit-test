69f33bd65e30815847c9b7c4859d83183a3e02e5
brainysoon <brainysoon@gmail.com>
2018-04-21 13:35:27 +0800

    [ GameControllerTest ] - Verify beginGame trigger gameView.showBegin()

--- src/test/java/tw/controllers/GameControllerTest.java
+++ src/test/java/tw/controllers/GameControllerTest.java
@@ -1,8 +1,35 @@
 package tw.controllers;
 
+import org.junit.Before;
+import org.junit.Test;
+import tw.core.Game;
+import tw.views.GameView;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 /**
  * 在GameControllerTest文件中完成GameController中对应的单元测试
  */
 public class GameControllerTest {
 
+    private GameController gameController;
+    private Game mockGame;
+    private GameView mockGameView;
+
+    @Before
+    public void setUp() {
+
+        mockGame = mock(Game.class);
+        mockGameView = mock(GameView.class);
+
+        gameController = new GameController(mockGame, mockGameView);
+    }
+
+    @Test
+    public void should_trigger_show_begin_game_when_call_begin_game() throws Exception {
+        gameController.beginGame();
+
+        verify(mockGameView).showBegin();
+    }
 }
