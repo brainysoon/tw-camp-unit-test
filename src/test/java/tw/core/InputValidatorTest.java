92987e9b2c9348b5c2d34ae7f1c6ba675f5d2271
brainysoon <brainysoon@gmail.com>
2018-04-21 15:35:44 +0800

    [ InputValidator ] - validate -  Verify should return true if num string is valid.

--- src/test/java/tw/core/InputValidatorTest.java
+++ src/test/java/tw/core/InputValidatorTest.java
@@ -1,8 +1,27 @@
 package tw.core;
 
+import org.junit.Before;
+import org.junit.Test;
+import tw.validator.InputValidator;
+
+import static org.junit.Assert.assertTrue;
+
 /**
  * 在InputValidatorTest文件中完成InputValidator中对应的单元测试
  */
 public class InputValidatorTest {
 
+    private InputValidator inputValidator;
+
+    @Before
+    public void setUp() throws Exception {
+
+        inputValidator = new InputValidator();
+    }
+
+    @Test
+    public void should_return_true_if_num_string_is_valid() {
+
+        assertTrue(inputValidator.validate("1 2 3 4"));
+    }
 }
