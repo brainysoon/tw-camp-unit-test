b33a553170aec7c29361b9d46276cb5e6526a0b4
brainysoon <brainysoon@gmail.com>
2018-04-21 14:39:44 +0800

    [ RandomIntGeneratorTest ] - Verify generateNums method should return an legal random number str.

--- src/test/java/tw/core/generator/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/generator/RandomIntGeneratorTest.java
@@ -1,5 +1,6 @@
 package tw.core.generator;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,5 +17,12 @@ public class RandomIntGeneratorTest {
     @Test(expected = IllegalArgumentException.class)
     public void should_throw_IllegalArgumentException_when_digitmax_less_thant_numbersOfNeed() {
         randomIntGenerator.generateNums(4, 9);
+    }
+
+    @Test
+    public void should_generate_an_array_of_number() {
+        String randomNumber = randomIntGenerator.generateNums(9, 4);
+
+        Assert.assertEquals(4, randomNumber.split(" ").length);
     }
 }
